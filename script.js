097f1483756620d3d3896e9bf8e63fa65427be2d
PBnJK <pedrobuitrago2007yahoo@gmail.com>
2026-01-15 15:47:23 -0300

    add: physics, pegs

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /* Main Plinko Script */
 
-const GRAVITY = 9.81;
+const GRAVITY = 0.5;
 
 /* A generic sphere collider */
 class SphereCollider {
@@ -24,11 +24,12 @@ class SphereCollider {
    * @param {SphereCollider} sphere
    */
   isCollidingWithSphere(sphere) {
-    const distance = Math.sqrt(
-      (sphere.x - this.x) ** 2 + (sphere.y - this.y) ** 2,
-    );
+    const xdist = sphere.x - this.x;
+    const ydist = sphere.y - this.y;
+    const distance = xdist * xdist + ydist * ydist;
 
-    return distance <= this.radius + sphere.radius;
+    const r = (this.radius + sphere.radius) * 2;
+    return distance <= r;
   }
 
   /**
@@ -105,21 +106,49 @@ class Ball {
    * @param {number} x
    * @param {number} y
    * @param {number} radius
+   * @param {number} mass
    * @param {string} color
    */
-  constructor(x, y, radius, color) {
+  constructor(x, y, radius, mass, color) {
     this.x = x;
     this.y = y;
     this.radius = radius;
+    this.mass = mass;
     this.color = color;
 
     this.ax = 0.0;
     this.ay = 0.0;
+
+    this.collider = new SphereCollider(this.x, this.y, this.radius);
   }
 
   /* Updates the physics of the ball */
   update(dt) {
-    this.ay += GRAVITY * dt;
+    this.ay = Math.min(this.ay + GRAVITY, 50.0);
+
+    this.x += this.ax * dt;
+    this.y += this.ay * dt;
+
+    this.collider.x = this.x;
+    this.collider.y = this.y;
+
+    if (this.x > 440 || this.x < 184) {
+      this.ax *= -1;
+    }
+
+    for (const peg of pegs) {
+      if (this.collider.isCollidingWithSphere(peg.collider)) {
+        this.#computeCollisionWithBall(peg);
+
+        this.x += this.ax * dt;
+        this.y += this.ay * dt;
+      }
+    }
+
+    for (const hole of holes) {
+      if (this.collider.isCollidingWithRect(hole.collider)) {
+      }
+    }
   }
 
   /**
@@ -134,6 +163,71 @@ class Ball {
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
     ctx.fill();
   }
+
+  /**
+   * Computes the collision between balls
+   * Based on:
+   *   https://en.wikipedia.org/wiki/Elastic_collision#Two-dimensional_collision_with_two_moving_objects
+   *
+   * @param {Ball} ball
+   */
+  #computeCollisionWithBall(ball) {
+    const cdiff_x = this.x - ball.x;
+    const cdiff_y = this.y - ball.y;
+
+    const vdiff_x = -this.ax;
+    const vdiff_y = -this.ay;
+
+    const dot = vdiff_x * cdiff_x + vdiff_y * cdiff_y;
+    if (dot > 0) {
+      const distance = cdiff_x * cdiff_x + cdiff_y * cdiff_y;
+      const scale = dot / distance;
+
+      const xc = cdiff_x * scale;
+      const yc = cdiff_y * scale;
+
+      const mass = this.mass + ball.mass;
+      const w = (2 * ball.mass) / mass;
+
+      this.ax += w * xc;
+      this.ay += w * yc;
+    }
+  }
 }
 
-/* A plinko peg */
+const pegDist = 16;
+const pegCount = 16;
+const pegs = [];
+
+for (let dy = 0; dy < 16; ++dy) {
+  const y = 32 + 16 * dy;
+
+  let bx = 320 - pegDist * (pegCount / 2);
+  if (dy % 2) {
+    bx -= pegDist / 2;
+  }
+
+  for (let dx = 0; dx < pegCount + (dy % 2); ++dx) {
+    const x = bx + pegDist * dx;
+    pegs.push(new Ball(x, y, 2, 100, "grey"));
+  }
+}
+
+/* Holes which tick each number */
+const holes = [];
+
+const ball = new Ball(320, 4, 4, 25, "red");
+ball.ax += Math.random() * 4 - 2;
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+window.setInterval(() => {
+  ctx.clearRect(0, 0, 600, 600);
+
+  ball.update(0.1667);
+  ball.draw(ctx);
+  for (const peg of pegs) {
+    peg.draw(ctx);
+  }
+}, 16);
